bdb49d4b79461e9b234f2c8aba7d0e6118e4c723
Amit <baghlaamit06@gmail.com>
2024-11-10 18:40:50 +0530

    save these changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-black text-white underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
